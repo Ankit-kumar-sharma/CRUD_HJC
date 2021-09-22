6f3b16814759dad7e6491c6c0f2be30a524a7c39
Ankit-kumar-sharma <ankitkumarsharmacse@gmail.com>
2021-09-22 17:54:14 +0530

    done

---
+++ config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDGuYIwbabT2ZJ0YO7OFOr_gOmSUNNE4WU",
+    authDomain: "crud-8ff8a.firebaseapp.com",
+    databaseURL: "https://crud-8ff8a-default-rtdb.firebaseio.com",
+    projectId: "crud-8ff8a",
+    storageBucket: "crud-8ff8a.appspot.com",
+    messagingSenderId: "738145793938",
+    appId: "1:738145793938:web:9ff10ba3ddfd3974e3b34b"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
